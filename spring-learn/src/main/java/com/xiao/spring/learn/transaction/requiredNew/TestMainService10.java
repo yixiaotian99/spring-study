e26a7c178549ae56da322a47684ffeee87475e5b
sunjinwei4 <sunjinwei@jd.com>
2021-01-24 20:24:26 +0800

    spring事务REQUIRED_NEW支持

---
+++ spring-learn/src/main/java/com/xiao/spring/learn/transaction/requiredNew/TestMainService10.java
@@ -0,0 +1,70 @@
+package com.xiao.spring.learn.transaction.requiredNew;
+
+import com.xiao.spring.learn.transaction.ServiceA;
+import com.xiao.spring.learn.transaction.ServiceB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @Author sunjinwei
+ * @Date 1/24/21 2:53 PM
+ * @Description TODO
+ * @see https://zhuanlan.zhihu.com/p/148504094
+ **/
+@Service
+public class TestMainService10 {
+
+    @Autowired
+    private ServiceA serviceA;
+
+    @Autowired
+    private TestService testService;
+
+    /**
+     * 使用 REQUIRED_NEW 事务
+     * 创建一个新的事务，如果当前存在事务就挂起当前事务
+     * <p>
+     * <p>
+     * 结论：节点1回滚，节点2、节点3插入成功
+     * 调用链条 testMainHasException-->内部调用 testNoException
+     * 前者有 REQUIRED 事务，后者有 REQUIRED_NEW 事务，执行时会将前者事务挂起，后者创建一个新事务
+     * 后者新事务提交并不受前者事务的影响，执行成功
+     * 前者因为事务被挂起，等于后者执行完成后事务继续执行，发生异常事务回滚
+     */
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public void testMainHasException() {
+        serviceA.testA("异常前A插入");   //插入回滚  节点1
+        testService.testNoException();   //service方法外部的调用，REQUIRED_NEW才会起作用，因为 Spring AOP切面特性
+        int i = 1 / 0;   //模拟出现异常
+    }
+
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
+
+        TestMainService10 testMainService = (TestMainService10) context.getBean("testMainService10");
+        testMainService.testMainHasException();
+    }
+}
+
+@Service
+class TestService {
+
+
+    @Autowired
+    private ServiceB serviceB;
+
+    /**
+     * 多次调用 serviceB 方法
+     */
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
+    public void testNoException() {
+        serviceB.testB("前面有异常B1插入");   //插入成功  节点2
+
+        serviceB.testB("前面有异常B2插入");   //插入成功  节点3
+    }
+}
